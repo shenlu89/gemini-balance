cbed5948b180c34b1b0ac4cc07487910cf053ee6
Shen Lu <shenlu89dev@gmail.com>
2025-08-23 01:15:14 +0800

    Fix Next.js config option

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@libsql/client'],
+  serverComponentsExternalPackages: ['@libsql/client'],
   env: {
     TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
     TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
