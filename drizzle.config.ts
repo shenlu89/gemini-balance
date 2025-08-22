1f6431fc354e134c4df9b441e860ec325ff8a182
Shen Lu <shenlu89dev@gmail.com>
2025-08-23 00:57:09 +0800

    Remove driver property from Drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,6 @@ export default {
   schema: './src/lib/db/schema.ts',
   out: './drizzle',
   dialect: 'sqlite',
-  driver: 'libsql',
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN!,
