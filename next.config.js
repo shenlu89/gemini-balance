f531b63d9c5331c5b73c4dd352c49f10d35fd57a
Shen Lu <shenlu89dev@gmail.com>
2025-08-27 08:50:17 +0800

    Fix Next.js config and 404 error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverComponentsExternalPackages: ['@libsql/client'],
   env: {
     TURSO_DATABASE_URL: process.env.TURSO_DATABASE_URL,
     TURSO_AUTH_TOKEN: process.env.TURSO_AUTH_TOKEN,
