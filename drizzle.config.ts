6ee94cc42f9a358b0933e6b0f2f9150da2cebbf4
Shen Lu <shenlu89dev@gmail.com>
2025-08-27 08:51:42 +0800

    Fix Drizzle config to use Turso driver

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'drizzle-kit'
 export default {
   schema: './src/lib/db/schema.ts',
   out: './drizzle',
-  dialect: 'sqlite',
+  driver: 'turso',
   dbCredentials: {
     url: `${process.env.TURSO_DATABASE_URL!}?authToken=${process.env.TURSO_AUTH_TOKEN!}`,
   },
