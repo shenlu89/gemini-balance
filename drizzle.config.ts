34a99330748bbaeaa893f3d194052b194f0041c1
Shen Lu <shenlu89dev@gmail.com>
2025-08-27 08:58:48 +0800

    Fix Drizzle config by separating auth token from URL

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,7 @@ export default {
   out: './drizzle',
   dialect: 'sqlite',
   dbCredentials: {
-    url: `${process.env.TURSO_DATABASE_URL!}?authToken=${process.env.TURSO_AUTH_TOKEN!}`,
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
   },
 } satisfies Config
